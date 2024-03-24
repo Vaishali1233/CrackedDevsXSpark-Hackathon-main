aa9fd1ea2cb8b841ecc8793fdec66d05c7d9e251
Vaishali <86144277+Vaishali1233@users.noreply.github.com>
2024-03-24 23:20:22 +0530

    Update login.jsx

--- src/(auth)/login.jsx
+++ src/(auth)/login.jsx
@@ -1,23 +1,12 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_API_KEY,
-//   authDomain: "footprint-friend-90ccd.firebaseapp.com",
-//   projectId: "footprint-friend-90ccd",
-//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_MESSAGE_ID,
-//   appId: process.env.REACT_APP_AP_ID,
-//   measurementId: process.env.REACT_APP_MEASUREMENT_ID,
-// };
-// Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+// Initialize Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyCkfz0KtGjPa4t_jfLFEg0lQiervLst4d0",
   authDomain: "safar-1b9ec.firebaseapp.com",
@@ -27,10 +16,6 @@ const firebaseConfig = {
   appId: "1:589292038744:web:1186f0854985d02749782d",
   measurementId: "G-61FZ4627RM"
 };
-
-// Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
@@ -61,3 +46,10 @@ export const signInWithGoogle = () => {
       });
   });
 };
+
+// HERE Maps API Initialization
+const platform = new H.service.Platform({
+  apikey: 'wX8XPC8nafBavveQLDl9NxshWXONNrU4oemMWlNQ_iI'
+});
+
+// Use platform to access other HERE Maps API services like geolocation, routing, etc.
